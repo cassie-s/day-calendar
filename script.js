0bd188e7ac018e787ebc0f01a964d03872fe3e84
cassie-s <56046933+cassie-s@users.noreply.github.com>
2021-09-16 16:11:55 -0500

    update

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+//load today's date//
+
 const clock = document.getElementById('clock');
 
 function updateTime() {
     const now = moment();
-    const humanReadable = now.format('ll');
+    const humanReadable = now.format('LL');
 
     clock.textContent = "Today's date is: " + humanReadable; 
 };
 
 setInterval(updateTime, 1000);
 updateTime();
+
+//make divs editable//
+
+function divClicked() {
+    var divHtml = $(this.html)
+}
+
+//conditionally format divs based present, past, or future (moment.js)//
+
+//when the save button is clicked, text is saved in local storage//
+
+
