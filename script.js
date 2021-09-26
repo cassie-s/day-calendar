1f6a4e913026426375338db6232256dc025485e8
cassie-s <56046933+cassie-s@users.noreply.github.com>
2021-09-26 09:21:31 -0500

    updated local storage code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //load today's date//
-
 const clock = document.getElementById('clock');
 
 function updateTime() {
@@ -12,14 +11,45 @@ function updateTime() {
 setInterval(updateTime, 1000);
 updateTime();
 
-//make divs editable//
+//Change textarea background color based on time
+var checkTime = function () {
 
-function divClicked() {
-    var divHtml = $(this.html)
-}
+    //Get Current time
+    var currentTime = moment().format('H');
 
-//conditionally format divs based present, past, or future (moment.js)//
+    //get all elements with class "taskarea"
+    var timeBlockElements = $(".textarea");
+
+    //loop through taskarea classes
+    for (var i = 0 ; i < timeBlockElements.length ; i++) {
+
+        //Get element i's ID as a string
+        var elementID = timeBlockElements[i].id;
+
+        //get element by ID
+        var manipID = document.getElementById(timeBlockElements[i].id)
+
+        //remove any old classes from element
+        $(timeBlockElements[i].id).removeClass(".present .past .future");
+
+        // apply new class if task is present/past/future
+        if (elementID < currentTime) {
+            $(manipID).addClass("past");
+        } else if (elementID > currentTime) {
+            $(manipID).addClass("future");
+        } else {
+            $(manipID).addClass("present");
+        }
+    }
+};
+
+// checkTime every 5 minutes
+setInterval(checkTime(), (1000 * 60) * 5);
+
 
 //when the save button is clicked, text is saved in local storage//
-
+var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks);
+};
 
